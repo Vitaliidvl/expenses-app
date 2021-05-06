9d47899305badf82b00f3649e8843368e6ea5ec6
Vitalii <kicker198686@gmail.com>
2021-05-06 23:08:28 +0300

    working whith list  and contition content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,37 @@
+import { useState } from 'react';
 import './NewExpense.css';
 
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseDataHandler = (enteredExData) => {
     const exData = {
       ...enteredExData,
       id: Math.random().toString(),
     };
     props.onAddExpense(exData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
